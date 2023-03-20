c4e9744d23d991e4517aef9169ee312b372cb3d4
MuriloJLima <murilojacobinalima12@gmail.com>
2023-03-20 15:07:31 -0300

    criação de categorias

--- routes/categoria.js
+++ routes/categoria.js
@@ -15,24 +15,79 @@ const models = require('../models')
 
 //armazenando models em constantes
 const categoria = models.Categoria
+const orcamento = models.Orcamento
 
+//rota com função de listar renda disponível para criação de categoria
+router.get('/disponivelCat', async (req, res) => {
+    try {
+        let id = req.body.id
+
+        let response = await orcamento.findByPk(id)
+
+        let soma = await categoria.sum('valor', { where: { orcamentoId: id } })
+        let rendaDisponivel =  response.valor - soma
+
+        res.send(JSON.stringify(rendaDisponivel))
+    } catch {
+        res.send(JSON.stringify('error'))
+    }
+})
+
+
+//rota com função de adicionar orçamento
 router.post('/adicionar', async (req, res) => {
-    await categoria.create({
-        orcamentoId: req.body.id,
-        nome: req.body.nome,
-        descricao: req.body.descricao,
-        valor: req.body.valor,
-    })  
-    res.send("success")
 
+    try {
+        //capturar o orçamento pelo id
+        let id = req.body.id
+
+        //listar o orçamento para capturar valor
+        let response = await orcamento.findByPk(id,
+            {
+                // include: [{
+                //     all: true
+                //     // model: categoria,
+                //     // required: true
+                // }]
+            }
+        )
+
+        //capturando a soma dos valores das categorias
+        let soma = await categoria.sum('valor', { where: { orcamentoId: id } })
+
+        //subtraindo o valor do orçamento com os valores das categorias para verificar a renda disponível
+        let rendaDisponivel = response.valor - soma
+
+        // verificar se o valor da categoria ultrapassa o orçamento e a renda disponível
+        let erros = []
+
+        if (rendaDisponivel < req.body.valor || response.valor < req.body.valor || !req.body.valor || req.body.valor == null) {
+            erros.push({ texto: "Valor inválido" })
+        }
+
+        if (erros.length > 0) {
+            res.send({ erros: erros })
+        } else {
+            //caso não ultrapasse, criar categoria
+            await categoria.create({
+                orcamentoId: req.body.id,
+                nome: req.body.nome,
+                descricao: req.body.descricao,
+                valor: req.body.valor,
+            })
+            res.send(JSON.stringify("success"))
+        }
+    } catch {
+        res.send(JSON.stringify('error'))
+    }
 })
 
 router.get('/listar', async (req, res) => {
-    await categoria.findAll().then((response)=>{
+    await categoria.findAll().then((response) => {
         res.send(response)
     })
 
-    
+
 })
 
 router.post('/editar', async (req, res) => {
--- routes/orcamento.js
+++ routes/orcamento.js
@@ -20,46 +20,54 @@ const orcamento = models.Orcamento
 //rota com função de adicionar orçamento
 router.post('/adicionar', async (req, res) => {
 
+    // await orcamento.create({
+
+    //     id: 1,
+    //     usuarioId: req.body.usuarioId,
+    //     valor: req.body.valor
+    // })
+
     //validação dos orçamentos
-    let erros = []
+    // let erros = []
 
-    if (req.body.valor < 500 || req.body.valor > 12000 || typeof req.body.valor == undefined ||
-        req.body.valor == null || !req.body.valor) {
-        erros.push({ texto: "Valor inválido" })
-    }
+    // if (req.body.valor < 500 || req.body.valor > 12000 || typeof req.body.valor == undefined ||
+    //     req.body.valor == null || !req.body.valor) {
+    //     erros.push({ texto: "Valor inválido" })
+    // }
 
-    if (erros.length > 0) {
-        res.send({ erros: erros })
-    } else {
+    // if (erros.length > 0) {
+    //     res.send({ erros: erros })
+    // } else {
 
-        //verificando se o usuário ja possui orçamento
-        let id = req.body.id
+    //verificando se o usuário ja possui orçamento
+    let id = req.body.usuarioId
 
-        await usuario.findByPk(id, { include: [{ all: true }] }).then((response) => {
+    await usuario.findByPk(id, { include: [{ all: true }] }).then((response) => {
 
-            if (response.Orcamento) {
-                res.send(JSON.stringify('Usuário ja possui orçamento'))
-            } else {
-                orcamento.create({
+        if (response.Orcamento) {
+            res.send(JSON.stringify('Usuário ja possui orçamento'))
+        } else {
+            orcamento.create({
 
-                    usuarioId: req.body.id,
-                    valor: req.body.valor
-                })
+                id: 1,
+                usuarioId: req.body.usuarioId,
+                valor: req.body.valor
+            })
 
-                res.send(JSON.stringify('success'))
-            }
-        }).catch((error) => {
-            res.send(JSON.stringify('error'))
-            console.log(error)
-        })
+            res.send(JSON.stringify('success'))
+        }
+    }).catch((error) => {
+        res.send(JSON.stringify('error'))
+        console.log(error)
+    })
 
-    }
+    // }
 })
 
 //rota com função de listar orçamento, que está relaionado ao usuario, 
 router.get('/listar', async (req, res) => {
 
-    let id = req.body.id
+    let id = req.body.usuarioId
 
     await usuario.findByPk(id, { include: [{ all: true }] }).then((response) => {
         res.send(response.Orcamento)
@@ -72,7 +80,7 @@ router.get('/listar', async (req, res) => {
 //rota com função de listar orçamento a ser editado
 router.get('/editar', async (req, res) => {
 
-    let id = req.body.id
+    let id = req.body.usuarioId
 
     await usuario.findByPk(id, { include: [{ all: true }] }).then((response) => {
         res.send(response.Orcamento)
@@ -86,37 +94,24 @@ router.get('/editar', async (req, res) => {
 //rota com função de editar orçamento
 router.post('/editar', async (req, res) => {
 
-    //validação dos orçamentos
-    let erros = []
+    let id = req.body.usuarioId
+    let valor = req.body.valor
 
-    if (req.body.valor < 500 || req.body.valor > 12500 || typeof req.body.valor == undefined ||
-        req.body.valor == null || !req.body.valor) {
-        erros.push({ texto: "Valor inválido" })
-    }
-
-    if (erros.length > 0) {
-        res.send({ erros: erros })
-    } else {
-
-        let id = req.body.id
-        let valor = req.body.valor
-
-        await orcamento.update(
-            { valor },
-            { where: { id } }
-        ).then(() => {
-            res.send(JSON.stringify('success'))
-        }).catch((error) => {
-            res.send(JSON.stringify('error'))
-            console.log(error)
-        })
-    }
+    await orcamento.update(
+        { valor },
+        { where: { id } }
+    ).then(() => {
+        res.send(JSON.stringify('success'))
+    }).catch((error) => {
+        res.send(JSON.stringify('error'))
+        console.log(error)
+    })
 })
 
 //rota com função de deletar orçamento
-// router.get('/deletar/:id', async (req, res) => {
-//     orcamento.destroy({ where: { id: req.params.id } })
-//     res.send('renda deletada')
-// })
+router.get('/deletar/:id', async (req, res) => {
+    orcamento.destroy({ where: { id: req.params.id } })
+    res.send('renda deletada')
+})
 
 module.exports = router
